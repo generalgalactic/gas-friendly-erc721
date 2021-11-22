078bb8eeb78dff25e613cbfac26bb7cef886aa34
Jacob DeHart <jacobd@jacobdehart.com>
2021-11-22 14:14:21 -0600

    use 1 test… probably incorrectly

--- test/test-friendly.js
+++ test/test-friendly.js
@@ -1,20 +1,25 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("FriendlyNFT", function () {
-  let contractFactory;
-  let owner;
-  let signers;
+["FriendlyNFT", "UnfriendlyNFT"].forEach((CONTRACT) => {
+  describe(CONTRACT, function () {
+    let contractFactory;
+    let owner;
+    let signers;
 
-  beforeEach(async () => {
-    [owner, ...signers] = await ethers.getSigners();
-    contractFactory = await ethers.getContractFactory("FriendlyNFT");
-  });
+    beforeEach(async () => {
+      [owner, ...signers] = await ethers.getSigners();
+      contractFactory = await ethers.getContractFactory(CONTRACT);
+    });
 
-  describe("Friendly NFT", async function () {
-    it("Should do something", async () => {
-      contract = await contractFactory.deploy();
-      await contract.safeMint(signers[0].address);
+    describe("Friendly NFT", async function () {
+      it("Can Mint", async () => {
+        contract = await contractFactory.deploy();
+        await expect(contract.safeMint(signers[0].address)).to.emit(
+          contract,
+          "Transfer"
+        );
+      });
     });
   });
 });
